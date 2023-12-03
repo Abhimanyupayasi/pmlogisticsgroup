8f5643c1de1e9de8f5b5aabb3dac66511b42f4e0
Abhimanyu Payasi <117967027+Abhimanyupayasi@users.noreply.github.com>
2023-12-03 19:22:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -181,23 +181,6 @@ document.querySelector(".menu-bar2").addEventListener("click",function(){
     window.location.href = './about.html';
 })
 
-// document.getElementById('loginForm').addEventListener('submit', function(event) {
-//     event.preventDefault(); // Prevents the form from submitting the traditional way
-
-//     // Get values from input fields
-//     var enteredId = document.getElementById('id').value;
-//     var enteredPassword = document.getElementById('password').value;
-
-//     // Check if entered ID and password match the specified values
-//     if (enteredId === '123456654' && enteredPassword === '123456') {
-//         // Redirect to the next page (replace 'next-page.html' with the actual URL)
-//         window.location.href = './next-page.html';
-//     } else {
-//         alert('Invalid ID or password. Please try again.'); // Show an error message
-//     }
-// });
- // Get the button element by its id
-
 
 
 
